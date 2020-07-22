b6a0241afbfd68249193e64b83f8caee9dd0365f
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-07-22 15:11:07 +0100

    tested Note.all and passed

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -6,15 +6,13 @@ it('>>Should be able to return the text that it is created with',function()
 
 it('>>This test should pass as it is checking the given out does NOT match the actual output',function()
   {
-    note = new Note ("This is my note")
-    expect(note.text).toNotEqual("This is not my note")
+    note1 = new Note ("This is another note")
+    expect(note1.text).toNotEqual("This is not my note")
 });
 
 it('>>Should return an array of notes', function(){
-  note = new Note ("This is my note")
-  note1 = new Note ("This is another note")
-  note2 = new Note ("Hello I am a note")
-  note3 = new Note ("Je suis note")
+
   console.log(Note.all())
-  expect(Note.all()).toEqual(["This is my note", "This is another note", "Hello I am a note", "Je suis note"])
+  expect(Note.all().length).toEqual(2)
+  expect(Note.all()[0].text).toEqual("This is my note")
 });
