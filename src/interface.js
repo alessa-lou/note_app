e1fb02bb944863d8ca0434b546a03175f811c55f
Arav <arav.patel@gmail.com>
2020-07-24 14:23:21 +0100

    user 3 story fully implemented

--- src/interface.js
+++ src/interface.js
@@ -13,17 +13,19 @@ return location.hash.split("#")[1];
 function showNote(note) {
   document.getElementById("notes").innerHTML = Note.all()[note].text
   // create button
+};
+
+function createButton(){
   var btn = document.createElement("BUTTON");
   btn.innerHTML = "Go Back";
   btn.setAttribute("id","button")
-  var capture = document.getElementById("capture");
-  capture.appendChild(btn);
+  document.body.appendChild(btn);
 
   //trigger hash change
   btn.addEventListener("click", function(){
     window.location.hash = ""
   })
-};
+}
 
 function printEachNote(note){
   document.getElementById("notes").innerHTML += "<li>" + `<a href="#${note.id}">` + note.text.substring(0,20) + "</a>" + "</li>" + "<br>";
@@ -40,12 +42,13 @@ function removeButton(){
 }
 
 function displayChange(){
-  console.log("do I make it in here?" + window.location.hash)
+  // console.log("do I make it in here?" + window.location.hash)
   if(window.location.hash == ""){
     removeButton();
     document.getElementById("notes").innerHTML = "";
     Note.all().forEach(printEachNote);
   }else{
     showNoteForCurrentPage();
+    createButton();
   }
 }
